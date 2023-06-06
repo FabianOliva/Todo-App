2b5108db6b06173c52ba6d0a2ba46a5fd89a588a
FabianOliva <fabianoliva01@hotmail.com>
2023-06-06 19:22:44 -0300

    img patch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import "../../../public/bg-desktop-dark.jpg";
-import "../../../public/bg-desktop-light.jpg";
+import "./../public/bg-desktop-light.jpg";
+import "./../public/bg-desktop-dark.jpg";
 
 function App() {
   const [theme, setTheme] = useState("sun");
